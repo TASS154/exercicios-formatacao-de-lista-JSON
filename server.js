978eabd00aab3715a06e432b59f3d703c4754a13
TASS154 <121037641+TASS154@users.noreply.github.com>
2024-06-04 11:49:06 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,31 +5,32 @@ const app = express();
 
 const port = 3001
 
-const filmesPath = path.join(__dirname, 'filmes.json')
-const filmesData = fs.readFileSync(filmesPath, 'utf-8')
+const filmesPath = path.join(__dirname, 'filmes.json');
+const filmesData = fs.readFileSync(filmesPath, 'utf-8');
 const filmes = JSON.parse(filmesData);
 
-function criarCard(filme) {
-    return `
-    <div class="card mb-3">
-        <img src="${filme.cartaz}" class="card-img-top " alt="${filme.nome}" width: 100px height: 100px>
-        <div class="card-body">
-            <h5 class="card-title">${filme.titulo}</h5>
-            <p class="card-text">${filme.diretor}</p>
-            <p class="card-text">${filme.genero}</p>
-            <p class="card-text">${filme.ano}</p>
-        </div>
-        `;
-}
-
 app.get('/', (req, res) => {
-    const cardsHtml = filmes.map(filme => criarCard(filme)).join('');
-    const pageHtmlPath = path.join(__dirname, 'dadosfilme.html');
-    let pageHtml = fs.readFileSync(pageHtmlPath, 'utf-8')
-    pageHtml = pageHtml.replace('{{cardsHtml}}', cardsHtml);
-    res.send(pageHtml);
+    let carsTable = ''; 
+
+    filmes.forEach(filme => {
+
+
+        carsTable += `
+        <tr>
+            <td>${filme.titulo}</td>
+            <td>${filme.genero}</td>
+            <td>${filme.ano}</td>
+            <td><img src="${filme.cartaz}" alt="${filme.nome}" style="max-width: 100px;"></td>
+        </tr>
+        `;
+    });
+
+    const htmlContent = fs.readFileSync('dadosfilme.html', 'utf-8');
+    const finalHtml  = htmlContent.replace('{{carsTable}}', carsTable);
+
+    res.send(finalHtml);
 });
 
-app.listen(port, () => {
-    console.log(`Servidor iniciado em http://localhost:${port}`);
-});
+app.listen(port, () =>{
+    console.log(`Servidor iniciado em: http://localhost:${port}`)
+})
